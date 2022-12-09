71bd137f5b4cbf76bc78cf35b9d43f42edbd0cde
mthom26 <mthom2626@gmail.com>
2022-12-09 18:04:58 +0000

    Day 07 part 2

--- 07-no-space-left-on-device/src/main.rs
+++ 07-no-space-left-on-device/src/main.rs
@@ -1,11 +1,11 @@
-use std::{collections::HashMap, fs};
+use std::{collections::HashMap, fs, str::FromStr};
 
 fn main() {
     let input = fs::read_to_string("./input.txt").unwrap();
 
-    let mut path = vec!["root"];
+    let mut path = vec!["root".to_owned(), "/".to_owned()];
     let mut totals: HashMap<String, usize> = HashMap::new();
-    totals.insert("root".to_owned(), 0);
+    totals.insert("root/".to_owned(), 0);
 
     for line in input.lines().skip(2) {
         let parts: Vec<&str> = line.split(" ").collect();
@@ -18,6 +18,8 @@ fn main() {
                         // println!("{}", vec_to_str(&path));
                     }
                     p => {
+                        let mut p = String::from_str(p).unwrap();
+                        p.push_str("/");
                         path.push(p);
                         // println!("{}", vec_to_str(&path));
                     }
@@ -52,13 +54,30 @@ fn main() {
         .map(|(_, n)| **n)
         .sum();
 
+    // Part 2
+    let total_used = totals.get("root/").unwrap();
+    let unused = 70000000 - total_used;
+    let needed = 30000000 - unused;
+
+    // Didn't need the dir here only the size but it was already done...
+    let mut dir = "".to_owned();
+    let mut dir_size = 0;
+    for (p, n) in v.iter() {
+        if **n >= needed {
+            dir.push_str(p);
+            dir_size = **n;
+            break;
+        }
+    }
+
     println!("Part 1: {:?}", total);
+    println!("Part 2: {:?} - {}", dir, dir_size);
 }
 
-fn vec_to_strings(path: &Vec<&str>) -> Vec<String> {
-    let mut s = "root".to_owned();
+fn vec_to_strings(path: &Vec<String>) -> Vec<String> {
+    let mut s = "root/".to_owned();
     let mut v = vec![];
-    for p in path.iter().skip(1) {
+    for p in path.iter().skip(2) {
         v.push(s.clone());
         s.push_str(p);
     }
